a81f8b412044904e2ec6f153e059caeb0fc06026
CColon1 <sikz26300@gmail.com>
2015-08-31 13:13:01 -0400

    ..

---
+++ InternForum/public/javascripts/angularApp.js
@@ -0,0 +1,58 @@
+var app = angular.module('InternForum', ['ui.router'])
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider,$urlRouterProvider){
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'homeCtrl'//,
+          //resolve: {
+          //postPromise: ['posts', function(posts){
+          //  var x = posts.getAll();
+          //  return x;
+          //}]
+          //}
+        }) 
+        $stateProvider
+        .state('postDetails', {
+          url: '/post',
+          templateUrl: '/post.html',
+          controller: 'postCtrl'//,
+          //resolve: {
+          //postPromise: ['posts', function(posts){
+          //  var x = posts.getAll();
+          //  return x;
+          //}]
+          //}
+        })  
+        $stateProvider
+        .state('login', {
+          url: '/login',
+          templateUrl: '/login.html',
+          controller: 'loginCtrl'//,
+          //resolve: {
+          //postPromise: ['posts', function(posts){
+          //  var x = posts.getAll();
+          //  return x;
+          //}]
+          //}
+        })  
+        $stateProvider
+        .state('register', {
+          url: '/register',
+          templateUrl: '/register.html',
+          controller: 'registerCtrl'//,
+          //resolve: {
+          //postPromise: ['posts', function(posts){
+          //  var x = posts.getAll();
+          //  return x;
+          //}]
+          //}
+        })   
+    }
+
+
+]);
